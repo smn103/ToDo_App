86205ee584c5f2667e1edeb472f438d4023becb4
ARK <anaghashree.ark@gmail.com>
2021-05-25 17:35:14 +0530

    File updates

--- app.js
+++ app.js
@@ -39,7 +39,7 @@ app.post("/removetask", function(req, res) {
 
 //render the ejs and display added task, completed task
 app.get("/", function(req, res) {
-    res.render("app", { task: task, complete: complete });
+    res.render("index", { task: task, complete: complete });
 });
 
 //set app to listen on port 5000
